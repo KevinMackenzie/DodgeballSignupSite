55fbf0c13dba65a8df142a53f98a7d37fc656756
Kevin Mackenzie <amberstudioprogrammer@gmail.com>
2015-09-27 13:16:14 -0400

    d

--- Login.php
+++ Login.php
@@ -1,43 +1,4 @@
-<?php
-include("cfg.php");
-session_start();
-if($_SERVER["REQUEST_METHOD"] == "POST")
-{
-if(isset($_POST['NewAccount']))
-{
-header("location: CreateNew.php");
-die();
-}
-else
-{
-// username and password sent from Form
-$myusername=mysqli_real_escape_string($db,$_POST['username']);
-$mypassword=mysqli_real_escape_string($db,$_POST['password']);
 
-$sql="SELECT Password FROM TeamLogin WHERE TeamID='$myusername';";
-$result=mysqli_query($db,$sql);
-$row=mysqli_fetch_array($result,MYSQLI_ASSOC);
-//$active=$row['active'];
-$count=mysqli_num_rows($result);
-
-if($count==1)
-{
-	if(password_verify($mypassword, mysqli_fetch_row($result)['Password']))
-	{
-		session_register("myusername");
-		$_SESSION['login_user']=$myusername;
-
-		header("location: Home.php");
-	}	
-}
-
-//this means something went wrong :(
-
-$error="Your Login ID or Password is Invalid";
-
-}
-}
-?>
 <form action="" method="post">
 <label>Team ID# :</label>
 <input type="text" name="username"/><br />
